c0e2d90b876b8cae3ee7d1157abd763f312f7c53
honkling <untitledgeese@gmail.com>
2024-08-23 09:02:23 -0500

    fix: allow @Ignore on files

--- common/src/main/kotlin/me/honkling/commando/common/annotations/Ignore.kt
+++ common/src/main/kotlin/me/honkling/commando/common/annotations/Ignore.kt
@@ -4,5 +4,5 @@ package me.honkling.commando.common.annotations
  * Tells commando to ignore the method.
  */
 @Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@Target(AnnotationTarget.FILE, AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
 annotation class Ignore()
