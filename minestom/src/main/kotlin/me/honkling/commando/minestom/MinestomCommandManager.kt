7b82854729ea52ff15c71792b24831219fe373fb
Colton <71715967+colton-boi@users.noreply.github.com>
2024-10-25 16:26:07 -0400

    fix: sender class validation

--- minestom/src/main/kotlin/me/honkling/commando/minestom/MinestomCommandManager.kt
+++ minestom/src/main/kotlin/me/honkling/commando/minestom/MinestomCommandManager.kt
@@ -52,7 +52,12 @@ class MinestomCommandManager(plugin: MinestomPlugin, debugMode: Boolean = false)
 
         val (subcommand, parameters) = result
         subcommand.method.isAccessible = true
-        
+
+        // Validate we're the correct sender
+        val senderType = subcommand.method.parameterTypes[0]
+        if (!senderType.isAssignableFrom(sender::class.java))
+            return false
+
         subcommand.method.invoke(null, sender, *parameters.toTypedArray())
 
         return true
